e4d31f64a2c66c1796fc0ac142ea653fd0134669
tsopgniduhamel <tsopgniduhamel@gmail.com>
2020-11-25 08:21:03 +0100

    testing the tutorial of ngx-markdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { SecurityContext } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
@@ -15,6 +19,29 @@ import 'hammerjs';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
+    HttpClientModule,
+    // MarkdownModule.forRoot(),
+    MarkdownModule.forRoot({
+      loader: HttpClient,
+    }),
+    MarkdownModule.forRoot({
+      sanitize: SecurityContext.NONE,
+    }),
+    // using specific options with ValueProvider and passing HttpClient
+    MarkdownModule.forRoot({
+      loader: HttpClient, // optional, only if you use [src] attribute
+      markedOptions: {
+        provide: MarkedOptions,
+        useValue: {
+          gfm: true,
+          breaks: false,
+          pedantic: false,
+          smartLists: true,
+          smartypants: false,
+        },
+      },
+    }),
+    MarkdownModule.forChild();
   ],
   providers: [],
   bootstrap: [AppComponent],
